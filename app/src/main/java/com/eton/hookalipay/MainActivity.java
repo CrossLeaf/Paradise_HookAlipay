90ce1dd0ccfe374b134650a3ae8b920cd4665a52
Eton <pk2p007@gmail.com>
2020-04-14 18:03:18 +0800

    測試 intent 至支付寶付款頁面

--- app/src/main/java/com/eton/hookalipay/MainActivity.java
+++ app/src/main/java/com/eton/hookalipay/MainActivity.java
@@ -7,6 +7,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,8 +19,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, isModuleActive() ? "module is active" : "module not active", Toast.LENGTH_SHORT).show();
 //        Uri uri = Uri.parse("https://qr.alipay.com/fkx17131t7ucml5ews4qx74?t=1586507412835");
-        Uri uri = Uri.parse("https://qr.alipay.com/fkx13028e1j1oirmrwthi70?t=1586847941929");
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//        Uri uri = Uri.parse("https://qr.alipay.com/fkx13028e1j1oirmrwthi70?t=1586847941929");
+//        Uri uri = Uri.parse("https://qr.alipay.com/fkx13028e1j1oirmrwthi70");
+//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//        String intentFullUri = "alipays://platformapi/startapp?appId=10000007&url=https://qr.alipay.com/fkx13028e1j1oirmrwthi70";
+//        try {
+//            // "intent://platformapi/startapp?saId=10000007&qrcode=https://qr.alipay.com/fkx13028e1j1oirmrwthi70?#Intent;scheme=alipays;package=com.eg.android.AlipayGphone;end"
+//            Intent intent = Intent.parseUri(intentFullUri, Intent.URI_INTENT_SCHEME);
+//            startActivity(intent);
+//        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+        intentToAlipay();
+    }
+
+    String qrCodeURL = "https://qr.alipay.com/fkx13028e1j1oirmrwthi70"; // 暫不支持此種方式，請在支付寶內打開操作
+
+    private void intentToAlipay() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = null;
+        try {
+            uri = Uri.parse("alipays://platformapi/startapp?saId=10000007&qrcode=" + URLEncoder.encode(qrCodeURL, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setData(uri);
         startActivity(intent);
     }
 
