d0db584c3ab75219b67acce8eec37402d2439c80
Eton <pk2p007@gmail.com>
2020-04-14 15:11:04 +0800

    使用 url 跳轉支付寶

--- app/src/main/java/com/eton/hookalipay/MainActivity.java
+++ app/src/main/java/com/eton/hookalipay/MainActivity.java
@@ -1,9 +1,11 @@
 package com.eton.hookalipay;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, isModuleActive() ? "module is active" : "module not active", Toast.LENGTH_SHORT).show();
+//        Uri uri = Uri.parse("https://qr.alipay.com/fkx17131t7ucml5ews4qx74?t=1586507412835");
+        Uri uri = Uri.parse("https://qr.alipay.com/fkx13028e1j1oirmrwthi70?t=1586847941929");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 
-    private boolean isModuleActive(){
+    private boolean isModuleActive() {
         return false;
     }
 }
