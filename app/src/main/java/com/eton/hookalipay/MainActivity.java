6fbac7835b1ebbe51ee242448e8d821827af1ad7
Eton <pk2p007@gmail.com>
2020-04-15 14:11:52 +0800

    測試使用 webView 做跳轉

--- app/src/main/java/com/eton/hookalipay/MainActivity.java
+++ app/src/main/java/com/eton/hookalipay/MainActivity.java
@@ -3,20 +3,25 @@ package com.eton.hookalipay;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
 import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+    String qrCodeURL = "https://qr.alipay.com/fkx13028e1j1oirmrwthi70"; // 暫不支持此種方式，請在支付寶內打開操作
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.wv_alipay);
         Toast.makeText(this, isModuleActive() ? "module is active" : "module not active", Toast.LENGTH_SHORT).show();
 //        Uri uri = Uri.parse("https://qr.alipay.com/fkx17131t7ucml5ews4qx74?t=1586507412835");
 //        Uri uri = Uri.parse("https://qr.alipay.com/fkx13028e1j1oirmrwthi70?t=1586847941929");
@@ -30,10 +35,9 @@ public class MainActivity extends AppCompatActivity {
 //        } catch (URISyntaxException e) {
 //            e.printStackTrace();
 //        }
-        intentToAlipay();
+//        intentToAlipay();
+        webViewToAlipay();
     }
-
-    String qrCodeURL = "https://qr.alipay.com/fkx13028e1j1oirmrwthi70"; // 暫不支持此種方式，請在支付寶內打開操作
 
     private void intentToAlipay() {
         Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -46,6 +50,42 @@ public class MainActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setData(uri);
         startActivity(intent);
+    }
+
+    private void webViewToAlipay() {
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // 設定可以訪問檔案
+        webSettings.setAllowFileAccess(true);
+        // 設定支援縮放
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        // webSettings.setDatabaseEnabled(true);
+        // 使用localStorage則必須開啟
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.contains("platformapi/startapp")) {
+                    try {
+                        Uri uri = Uri.parse(url);
+                        Intent intent;
+                        intent = Intent.parseUri(url,
+                                Intent.URI_INTENT_SCHEME);
+                        intent.addCategory("android.intent.category.BROWSABLE");
+                        intent.setComponent(null);
+                        intent.setSelector(null);
+                        startActivity(intent);
+                    } catch (Exception e) {
+                    }
+                } else {
+                    view.loadUrl(url);
+                }
+                return true;
+            }
+        });
+        webView.loadUrl(qrCodeURL);
     }
 
     private boolean isModuleActive() {
